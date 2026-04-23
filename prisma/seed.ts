24840f4d00b2feee0e9f2f5ede0199823a3d24af
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2026-04-23 12:53:34 +0200

    Seeding finaly works! Schema restraints added.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,21 +8,203 @@ const prisma = new PrismaClient({
     adapter
 });
 
-const userData: Prisma.UserCreateInput[] = [
-    {
-        email: "ada@lovelace.cyberspace",
-        name: "Ada Lovelace",
-    },
-    {
-        email: "hedy@lamarr.cyberspace",
-        name: "Hedy Lamarr"
-    }
-];
-
-export async function main(){
-    for (const u of userData){
-        await prisma.user.create({data:u});
-    }
+function daysAgo(days: number): Date {
+    const date = new Date();
+    date.setDate(date.getDate() - days);
+    return date;
 }
 
-main();
+async function main(){
+    const bee = await prisma.user.upsert({
+        where: { email: "queen@bee.hive" },
+        update: {},
+        create: {
+            createdAt: daysAgo(5),
+            email: "queen@bee.hive",
+            userName: "Queen Bee",
+            createdGroups: {
+                create: [
+                    {
+                        name: "Beehive",
+                    }
+                ]
+            }
+        },
+        include: {
+            createdGroups: true
+        }
+    });
+
+    const ada = await prisma.user.upsert({
+        where: { email: "ada@lovelace.cyberspace" },
+        update: {},
+        create: {
+            createdAt: daysAgo(5),
+            email: "ada@lovelace.cyberspace",
+            userName: "Ada Lovelace",
+            meetings: {
+                create: [
+                    {
+                        title: "Adas first meeting",
+                        womenCount: 3,
+                        nonbinaryCount: 3,
+                        menCount: 3,
+                        totalSpeakingTime: 3000,
+                        womenSpeakingTime: 1000,
+                        nonbinarySpeakingTime: 1000,
+                        menSpeakingTime: 1000,
+                        womenStatementCount: 5,
+                        nonbinaryStatementCount: 4,
+                        menStatementCount: 3
+                    }
+                ]
+            },
+            createdGroups: {
+                create: [
+                    {
+                        name: "Ada's fanclub",
+                        description: "Discussion forum",
+                    },
+                    {
+                        name: "Ada's bookclub",
+                    }
+                ]
+            }
+        },
+        include: {
+            createdGroups: true
+        }
+    });
+
+    const hedy = await prisma.user.upsert({
+        where: { email: "hedy@lamarr.cyberspace" },
+        update: {},
+        create: {
+            createdAt: daysAgo(5),
+            email: "hedy@lamarr.cyberspace",
+            userName: "Hedy Lamarr",
+            meetings: {
+                create: [
+                    {
+                        title: "Lamarr's llama limitations",
+                        womenCount: 3,
+                        nonbinaryCount: 4,
+                        menCount: 3,
+                        totalSpeakingTime: 3000,
+                        womenSpeakingTime: 1000,
+                        nonbinarySpeakingTime: 1000,
+                        menSpeakingTime: 1000,
+                        womenStatementCount: 5,
+                        nonbinaryStatementCount: 4,
+                        menStatementCount: 3
+                    },
+                    {
+                        title: "Important bizznizz",
+                        startedAt: daysAgo(1),
+                        womenCount: 3,
+                        nonbinaryCount: 3,
+                        totalSpeakingTime: 3000,
+                        womenSpeakingTime: 2000,
+                        nonbinarySpeakingTime: 1000,
+                        womenStatementCount: 5,
+                        nonbinaryStatementCount: 4,
+                    }
+                ]
+            },
+        },
+    });
+
+    const agatha = await prisma.user.upsert({
+        where: { email: "agatha@christie.crimesforsale" },
+        update: {},
+        create: {
+            createdAt: daysAgo(5),
+            email: "agatha@christie.crimesforsale",
+            userName: "Agatha Christie",
+        }
+    });
+
+    const bookclub = ada.createdGroups.find(g => g.name === "Ada's bookclub");
+    const beehive = bee.createdGroups.find(g => g.name === "Beehive");
+
+    await prisma.group.update({
+        where: {
+            creatorId_name: {
+                creatorId: bee.id,
+                name: "Beehive"
+            }
+        },
+        data: {
+            klockers: {
+                connect: [
+                    { id: ada.id },
+                    { id: hedy.id },
+                    { id: agatha.id },
+                ]
+            }
+        }
+    });
+
+    await prisma.group.update({
+        where: {
+            creatorId_name: {
+                creatorId: ada.id,
+                name: "Ada's bookclub"
+            }
+        },
+        data: {
+            klockers: {
+                connect: [
+                    { id: agatha.id },
+                ]
+            }
+        }
+    });
+
+    await prisma.meeting.create({
+        data: {
+            creatorId: agatha.id,
+            groupId: beehive!.id,
+            title: "Nursery rimes and how to use them",
+            womenCount: 3,
+            nonbinaryCount: 3,
+            menCount: 3,
+            totalSpeakingTime: 3000,
+            womenSpeakingTime: 1000,
+            nonbinarySpeakingTime: 1000,
+            menSpeakingTime: 1000,
+            womenStatementCount: 5,
+            nonbinaryStatementCount: 4,
+            menStatementCount: 3
+        }
+    })
+    await prisma.meeting.create({
+        data: {
+            createdAt: daysAgo(3),
+            startedAt: daysAgo(3),
+            creatorId: agatha.id,
+            groupId: bookclub!.id,
+            title: "Archaeology for dummies",
+            womenCount: 3,
+            nonbinaryCount: 3,
+            menCount: 3,
+            totalSpeakingTime: 3000,
+            womenSpeakingTime: 1000,
+            nonbinarySpeakingTime: 1000,
+            menSpeakingTime: 1000,
+            womenStatementCount: 5,
+            nonbinaryStatementCount: 4,
+            menStatementCount: 3
+        }
+    })
+}
+
+main()
+    .then(async ()=> {
+        await prisma.$disconnect();
+    })
+    .catch(async (e)=> {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    })
