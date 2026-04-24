a28eb7eb23ad4e60cbb7c251ba10956aebfed197
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2026-04-24 17:16:06 +0200

    BetterAuth and schemas and instance.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,5 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "../../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Prisma } from "@prisma/client/extension";
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
