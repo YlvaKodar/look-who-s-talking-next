caf0dea313b156904a840a7fbc4d2960c7a0bb84
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2026-04-29 10:34:38 +0200

    Test meetingroute.

---
+++ src/app/api/meetings/route.ts
@@ -0,0 +1,19 @@
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+    if(!session) {
+        return NextResponse.json({error: "No such session"}, { status: 401 });
+    }
+    const meetings = await prisma.meeting.findMany({
+        where: {creatorId: session.user.id}
+    });
+
+    return NextResponse.json(meetings);
+}
+
