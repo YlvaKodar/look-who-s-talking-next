fc9ab79a6624f13199ea492b640ef988a2ffc6da
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2026-05-06 17:16:57 +0200

    Group and Meeting POST route handler: try/catch added!

--- src/app/api/groups/route.ts
+++ src/app/api/groups/route.ts
@@ -2,15 +2,12 @@ import { auth } from "@/lib/auth";
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { prisma } from "@/lib/prisma";
+import {Prisma} from "@/generated/prisma/client";
 
 export async function GET( request: Request ) {
 
-    const session = await auth.api.getSession({
-        headers: await headers()
-    });
-    if(!session) {
-        return NextResponse.json({error: "No such session"}, { status: 401 });
-    }
+    const session = await auth.api.getSession({ headers: await headers() });
+    if(!session) return NextResponse.json({error: "No such session"}, { status: 401 });
 
     const url = new URL(request.url)
     const status = url.searchParams.get("status");
@@ -39,21 +36,28 @@ export async function GET( request: Request ) {
 
     return NextResponse.json(groups);
 }
+
 export async function POST(request: Request) {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    });
-    if(!session) {
-        return NextResponse.json({error: "No such session"}, { status: 401 });
-    }
+    const session = await auth.api.getSession({ headers: await headers() });
+    if(!session) return NextResponse.json({error: "No such session"}, { status: 401 });
+
     const groupData = await request.json();
 
-    const newGroup = await prisma.group.create({
-        data: {
-            ...groupData,
-            keeperId: session.user.id
+    try {
+        const newGroup = await prisma.group.create({
+            data: {
+                ...groupData,
+                keeperId: session.user.id
+            }
+        });
+        return NextResponse.json(newGroup, { status: 201 });
+    } catch (error){
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2006" || error.code === "P2007") return NextResponse.json({ error: "Invalid field format." }, { status : 400 });
+            if (error.code === "P2002") return NextResponse.json({ error: "The user might already keep a group by this name." }, { status : 409 });
+            if (error.code === "P2011") return NextResponse.json({ error: "A required field is missing." }, { status : 400 });
+            if (error.code === "P2003") return NextResponse.json({ error: "A referenced user or meeting does not exist." }, { status : 400 });
         }
-    });
-
-    return NextResponse.json(newGroup, { status: 201 });
+        return NextResponse.json({ error: "Ok, so ths didn't go as planned ..." }, { status: 500 })
+    }
 }
--- src/app/api/meetings/route.ts
+++ src/app/api/meetings/route.ts
@@ -2,14 +2,12 @@ import { auth } from "@/lib/auth";
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { prisma } from "@/lib/prisma";
+import {Prisma} from "@/generated/prisma/client";
 
 export async function GET() {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    });
-    if(!session) {
-        return NextResponse.json({error: "No such session"}, { status: 401 });
-    }
+    const session = await auth.api.getSession({ headers: await headers() });
+    if(!session) return NextResponse.json({error: "No such session"}, { status: 401 });
+
     const meetings = await prisma.meeting.findMany({
         where: {keeperId: session.user.id}
     });
@@ -18,20 +16,26 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    });
-    if(!session) {
-        return NextResponse.json({error: "No such session"}, { status: 401 });
-    }
+    const session = await auth.api.getSession({ headers: await headers() });
+    if(!session) return NextResponse.json({error: "No such session"}, { status: 401 });
+
     const meetingData = await request.json();
 
-    const newMeeting = await prisma.meeting.create({
-        data: {
-            ...meetingData,
-            keeperId: session.user.id
+    try {
+        const newMeeting = await prisma.meeting.create({
+            data: {
+                ...meetingData,
+                keeperId: session.user.id
+            }
+        });
+        return NextResponse.json(newMeeting, { status: 201 });
+    } catch (error){
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2006" || error.code === "P2007") return NextResponse.json({ error: "Invalid field format." }, { status : 400 });
+            if (error.code === "P2002") return NextResponse.json({ error: "The user might already keep a meeting with this start time." }, { status : 409 });
+            if (error.code === "P2011") return NextResponse.json({ error: "A required field is missing." }, { status : 400 });
+            if (error.code === "P2003") return NextResponse.json({ error: "A referenced user or group does not exist." }, { status : 400 });
         }
-    });
-
-    return NextResponse.json(newMeeting, { status: 201 });
+        return NextResponse.json({ error: "Ok, so ths didn't go as planned ..." }, { status: 500 })
+    }
 }
