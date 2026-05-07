7d2eef28191a498dea7c01f22ce97d2f9a037997
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2026-05-07 16:21:25 +0200

    Added data check and minifixes in meeting[id]

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -9,8 +9,8 @@ export async function GET(
     request: Request,
     { params }  : { params: Promise<{ id: string }> }
 ) {
-    const session = await auth.api.getSession({ headers: await headers() });
 
+    const session = await auth.api.getSession({ headers: await headers() });
     if (!session) return NextResponse.json({ error: "No such session" }, { status: 401 });
 
     if (session.user.role !== "ADMIN") return NextResponse.json({error: "THIS DECISION IS NOT UP TO YOU"}, { status: 403 });
@@ -42,13 +42,11 @@ export async function PUT(
     ) {
 
     const session = await auth.api.getSession({ headers: await headers() });
-
     if (!session) return NextResponse.json({ error: "No such session" }, { status: 401 });
 
     if (session.user.role !== "ADMIN") return NextResponse.json({error: "THIS DECISION IS NOT UP TO YOU"}, { status: 403 });
 
     const { id } = await params;
-
     const { email, name, role } = await request.json();
 
     const data: UserUpdateInput = {};
@@ -59,15 +57,13 @@ export async function PUT(
         if (role !== "ADMIN" && role !== "USER") return NextResponse.json({error: "Invalid role. Must be 'ADMIN' or 'USER'"}, { status: 400 });
 
         if (id === session.user.id && role === "USER") {
-            const adminCount = await prisma.user.count({
-                where: { role: "ADMIN" }
-            });
-
+            const adminCount = await prisma.user.count({  where: { role: "ADMIN" }   });
             if (adminCount === 1) return NextResponse.json({error: "A new admin must be assigned before this user's role can be updated."}, { status: 409 });
         }
-
         data.role = role;
     }
+
+    if (!(Object.keys(data).length)) return NextResponse.json({error: "No data provided"})
 
     try {
         const updatedUser = await prisma.user.update({
@@ -80,7 +76,7 @@ export async function PUT(
             if (error.code === "P2025") return NextResponse.json({ error: "User not found" }, { status : 404 });
             if (error.code === "P2002") return NextResponse.json({ error: "The name or email might not be available." }, { status : 409 });
         }
-        return NextResponse.json({ error: "Ok, so ths didn't go as planned ..." }, { status: 500 })
+        return NextResponse.json({ error: "Ok, so this didn't go as planned ..." }, { status: 500 })
     }
 }
 
@@ -90,19 +86,14 @@ export async function DELETE(
 ) {
 
     const session = await auth.api.getSession({ headers: await headers() });
-
     if(!session) return NextResponse.json({error: "No such session"}, { status: 401 });
-
     if (session.user.role !== "ADMIN") return NextResponse.json({error: "THIS DECISION IS NOT UP TO YOU"}, { status: 403 });
 
     const { id } = await params;
 
     if (id === session.user.id){
-        const adminCount = await prisma.user.count({
-            where: { role: "ADMIN" }
-        });
-
-        if (adminCount === 1) return NextResponse.json({error: "A new admin must be assigned before this user is deleted."}, { status: 409 });
+            const adminCount = await prisma.user.count({    where: { role: "ADMIN" }    });
+            if (adminCount === 1) return NextResponse.json({error: "A new admin must be assigned before this user is deleted."}, { status: 409 });
     }
 
     try {
@@ -116,6 +107,6 @@ export async function DELETE(
             if (error.code === "P2025") return NextResponse.json({ error: "User not found" }, { status : 404 });
             if (error.code === "P2003") return NextResponse.json({ error: "Account cannot be deleted unless all connected meetings and groups are deleted or assigned new keepers." }, { status : 409 });
         }
-        return NextResponse.json({ error: "Ok, so ths didn't go as planned ..." }, { status: 500 })
+        return NextResponse.json({ error: "Ok, so this didn't go as planned ..." }, { status: 500 })
     }
 }
