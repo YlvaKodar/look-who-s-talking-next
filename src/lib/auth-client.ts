7789ed68fd017a9c68cbac173f716a8925a90ded
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2026-05-21 00:11:09 +0200

    Forgot an env.

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.BETTER_AUTH_URL,
     plugins: [inferAdditionalFields<typeof auth>()],
 });
 
