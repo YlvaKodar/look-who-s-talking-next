60f9988041c062a6c1463998c5eeb0919f8928e0
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2026-06-18 20:01:21 +0200

    How to use page done for now

--- src/components/HowToUse.tsx
+++ src/components/HowToUse.tsx
@@ -1,42 +1,56 @@
 "use client"
-import {
-    BigSectionContainer,
-    ButtonContainer, AlertContainer,
-    ListButtonContainer,
-    SimpleContainer,
-    SmallSectionContainer
-} from "@/ui/Containers";
+import {BigSectionContainer, AlertContainer, SimpleContainer} from "@/ui/Containers";
 import {ChevronIcon, P} from "@/ui/Common";
 import {useState} from "react";
 import {H1, H3, H4, ColorHeading} from "@/ui/Headings";
 import {HowToUseText} from "@/constants/constants";
 
 export function HowToUse() {
+    const [showBasic, setShowBasic] = useState(false);
+    const [showAccount, setShowAccount] = useState(false);
     return (
         <BigSectionContainer>
             <ColorHeading heading={"HOW TO USE"}/>
-            <BasicUsage/>
-            <AccountUsage/>
+                <P color={"tip"} className={"font-bold text-center text-xl"}>{"Click the "}<ChevronIcon isOpen={false}/> { "to open or close the chapters!"}</P>
+            <div onClick={() => setShowBasic(!showBasic)}>
+                <H1 center={""}>{HowToUseText.basics} <ChevronIcon isOpen={showBasic}/></H1>
+            </div>
+            {showBasic && (
+                <BasicUsage/>
+            )}
+            <div onClick={() => setShowAccount(!showAccount)}>
+                <H1 center={""}>{HowToUseText.withAccount} <ChevronIcon isOpen={showAccount}/></H1>
+            </div>
+            {showAccount && (
+                <AccountUsage/>
+            )}
         </BigSectionContainer>
     )
 }
 
 function BasicUsage() {
-    const [showBasic, setShowBasic] = useState(false);
+    const [showOne, setShowOne] = useState(false);
+    const [showTwo, setShowTwo] = useState(false);
+    const [showThree, setShowThree] = useState(false);
     return (
         <div>
-            <div onClick={() => setShowBasic(!showBasic)}>
-                <H1 center={""}>{HowToUseText.basics} <ChevronIcon isOpen={showBasic}/></H1>
-            </div>
-            {showBasic && (
-                <>
-                    <SimpleContainer>
-                        <H3 center={""}>{HowToUseText.one}</H3>
+            <SimpleContainer>
+                <div onClick={() => setShowOne(!showOne)}>
+                    <H3 center={""}>{HowToUseText.one} <ChevronIcon isOpen={showOne}/></H3>
+                </div>
+                {showOne && (
+                    <>
                         <P>{HowToUseText.oneOne}</P>
                         <P>{HowToUseText.oneTwo}</P>
-                    </SimpleContainer>
-                    <SimpleContainer>
-                        <H3 center={""}>{HowToUseText.two}</H3>
+                    </>
+                )}
+            </SimpleContainer>
+            <SimpleContainer>
+                <div onClick={() => setShowTwo(!showTwo)}>
+                    <H3 center={""}>{HowToUseText.two} <ChevronIcon isOpen={showTwo}/></H3>
+                </div>
+                {showTwo && (
+                    <>
                         <P>{HowToUseText.twoOne}</P>
                         <P>{HowToUseText.twoTwo}</P>
                         <AlertContainer variant={"example"} >
@@ -54,9 +68,15 @@ function BasicUsage() {
                             <P>{HowToUseText.twoNoteTwo}</P>
                             <P>{HowToUseText.twoNoteThree}</P>
                         </AlertContainer>
-                    </SimpleContainer>
-                    <SimpleContainer>
-                        <H3 center={""}>{HowToUseText.three}</H3>
+                    </>
+                )}
+            </SimpleContainer>
+            <SimpleContainer>
+                <div onClick={() => setShowThree(!showThree)}>
+                    <H3 center={""}>{HowToUseText.three} <ChevronIcon isOpen={showThree}/></H3>
+                </div>
+                {showThree && (
+                    <>
                         <P>{HowToUseText.threeOne}</P>
                         <P>{HowToUseText.threeTwo}</P>
                         <P color={"tip"} className={"font-semibold"} >{HowToUseText.threeThree}</P>
@@ -65,73 +85,67 @@ function BasicUsage() {
                             <H4 color={"tip"}>{HowToUseText.tip}</H4>
                             <P>{HowToUseText.tipMore}</P>
                         </AlertContainer>
-                    </SimpleContainer>
-                    <SimpleContainer>
+                    </>
+                )}
+            </SimpleContainer>
+            <SimpleContainer>
+                {showThree && (
+                    <>
                         <H3 center={""}>{HowToUseText.lastly}</H3>
                         <P>{HowToUseText.lastlyOne}</P>
                         <P color={"danger"} className={"font-semibold"}>{HowToUseText.lastlyTwo}</P>
                         <P>{HowToUseText.lastlyThree}</P>
-                    </SimpleContainer>
-                </>
-            )}
+                    </>
+                )}
+            </SimpleContainer>
         </div>
 
     )
 }
 
 function AccountUsage() {
-    const [showAccount, setShowAccount] = useState(false);
     return (
         <div>
-            <div onClick={() => setShowAccount(!showAccount)}>
-                <H1 center={""}>{HowToUseText.withAccount} <ChevronIcon isOpen={showAccount}/></H1>
-            </div>
-
-            {showAccount && (
-                <>
-                    <SimpleContainer>
-                        <AlertContainer variant={"danger"}>
-                            <P>{HowToUseText.disclaimer}</P>
-                        </AlertContainer>
-                    </SimpleContainer>
-                    <SimpleContainer>
-                        <H3 center={""}>{HowToUseText.createAccount}</H3>
-                        <P>{HowToUseText.createAccountOne}</P>
-                        <P>{HowToUseText.createAccountTwo}</P>
-                    </SimpleContainer>
-                    <SimpleContainer>
-                        <H3 center={""}>{HowToUseText.groups}</H3>
-                        <P>{HowToUseText.groupsMore}</P>
-                        <AlertContainer variant={"alert"}>
-                            <H4 color={"alert"}>{HowToUseText.note}</H4>
-                            <P>{HowToUseText.groupsNote}</P>
-                        </AlertContainer>
-                        <AlertContainer variant={"example"}>
-                            <H4 color={"example"}>{HowToUseText.example}</H4>
-                            <P>{HowToUseText.groupsExampleOne}</P>
-                            <P>{HowToUseText.groupsExampleTwo}</P>
-                            <P>{HowToUseText.groupsExampleThree}</P>
-                            <P>{HowToUseText.groupsExampleFour}</P>
-                        </AlertContainer>
-                    </SimpleContainer>
-                    <SimpleContainer>
-                        <H3 center={""}>{HowToUseText.meetings}</H3>
-                        <P>{HowToUseText.meetingsOne}</P>
-                        <P>{HowToUseText.meetingsTwo}</P>
-                        <P>{HowToUseText.meetingsThree}</P>
-                        <AlertContainer variant={"alert"}>
-                            <H4 color={"alert"}>{HowToUseText.note}</H4>
-                            <P>{HowToUseText.meetingNote}</P>
-                        </AlertContainer>
-                    </SimpleContainer>
-                    <SimpleContainer>
-                        <H3 center={""}>{HowToUseText.upcoming}</H3>
-                        <P>{HowToUseText.upcomingOne}</P>
-                        <P>{HowToUseText.upcomingTwo}</P>
-                    </SimpleContainer>
-                </>
-            )
-            }
+            <SimpleContainer>
+                <AlertContainer variant={"danger"}>
+                    <P>{HowToUseText.disclaimer}</P>
+                </AlertContainer>
+            </SimpleContainer>
+            <SimpleContainer>
+                <H3 center={""}>{HowToUseText.createAccount}</H3>
+                <P>{HowToUseText.createAccountOne}</P>
+                <P>{HowToUseText.createAccountTwo}</P>
+            </SimpleContainer>
+            <SimpleContainer>
+                <H3 center={""}>{HowToUseText.groups}</H3>
+                <P>{HowToUseText.groupsMore}</P>
+                <AlertContainer variant={"alert"}>
+                    <H4 color={"alert"}>{HowToUseText.note}</H4>
+                    <P>{HowToUseText.groupsNote}</P>
+                </AlertContainer>
+                <AlertContainer variant={"example"}>
+                    <H4 color={"example"}>{HowToUseText.example}</H4>
+                    <P>{HowToUseText.groupsExampleOne}</P>
+                    <P>{HowToUseText.groupsExampleTwo}</P>
+                    <P>{HowToUseText.groupsExampleThree}</P>
+                    <P>{HowToUseText.groupsExampleFour}</P>
+                </AlertContainer>
+            </SimpleContainer>
+            <SimpleContainer>
+                <H3 center={""}>{HowToUseText.meetings}</H3>
+                <P>{HowToUseText.meetingsOne}</P>
+                <P>{HowToUseText.meetingsTwo}</P>
+                <P>{HowToUseText.meetingsThree}</P>
+                <AlertContainer variant={"alert"}>
+                    <H4 color={"alert"}>{HowToUseText.note}</H4>
+                    <P>{HowToUseText.meetingNote}</P>
+                </AlertContainer>
+            </SimpleContainer>
+            <SimpleContainer>
+                <H3 center={""}>{HowToUseText.upcoming}</H3>
+                <P>{HowToUseText.upcomingOne}</P>
+                <P>{HowToUseText.upcomingTwo}</P>
+            </SimpleContainer>
         </div>
     )
 }
